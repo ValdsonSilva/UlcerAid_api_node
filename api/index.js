5cd4aaa3766862ddb17d85a528a74c7bc633e814
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2025-03-18 09:59:32 -0300

    hotfix: Deploy

--- api/index.js
+++ api/index.js
@@ -6,4 +6,7 @@ const port = config.get("server.port");
 app.get("/", (req, resp) => resp.send("Express on Vercel"))
 
 
+// rodar aplicação -- ⚠️ Não usar app.listen() aqui na Vercel
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
+
 module.exports = app;
